186c33f7d250d5f78078be365f99778d5d3b77d9
Alexandre MENIGAULT <alexandre.menigault@gmail.com>
2025-09-03 15:26:39 +0200

    Add Burger and Product price migration

---
+++ EatDomicile.Core/Migrations/20250903132618_Burger_Product_Price.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EatDomicile.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Burger_Product_Price : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Burgers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Burgers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Burgers_Foods_Id",
+                        column: x => x.Id,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Burgers");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Products");
+        }
+    }
+}
