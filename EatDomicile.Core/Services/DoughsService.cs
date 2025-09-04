ad376f69a86aa94c8442f2c749b58938a8cf65ff
Alain Roger SANGA <alainroger.sanga@gmail.com>
2025-09-04 14:15:26 +0200

    add of Dough service

---
+++ EatDomicile.Core/Services/DoughsService.cs
@@ -0,0 +1,33 @@
+﻿
+using EatDomicile.Core.Contexts;
+using EatDomicile.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EatDomicile.Core.Services;
+
+public class DoughsService
+{
+     public DoughsService()
+    {
+
+    }
+    // Read all
+
+    public List<Dough> GetAllDoughs()
+    {
+        using var context = new ProductContext();
+        var DoughsList = context.Doughs.ToList();
+        return DoughsList;
+        
+    }
+
+    public Dough AddDough(Dough Dough)
+    {
+        using var context = new ProductContext();
+        context.Doughs.Add(Dough);
+        context.SaveChanges();
+        return Dough;
+    }
+
+}
